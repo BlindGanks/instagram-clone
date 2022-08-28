e354563b1f58c7b09b127df78f46e88d25db1554
blindganks <abdelhaktebak@gmail.com>
2022-08-28 12:46:44 +0100

    handled when user signs out from profile and adjusted header UI

--- components/Header.js
+++ components/Header.js
@@ -6,8 +6,9 @@ import {
   HeartIcon,
   PaperAirplaneIcon,
 } from "@heroicons/react/outline";
-import { HomeIcon } from "@heroicons/react/solid";
-import { Router, useRouter } from "next/router";
+import { HomeIcon as HomeIconSolid } from "@heroicons/react/solid";
+import { HomeIcon as HomeIconOutline } from "@heroicons/react/outline";
+import { useRouter } from "next/router";
 import { modalState } from "../atoms/modalAtom";
 import { useRecoilState } from "recoil";
 import { userState } from "../atoms/userAtom";
@@ -22,12 +23,12 @@ function Header() {
   const [visibleUserMenu, setVisibleUserMenu] = useState(false);
   const router = useRouter();
   return (
-    <div className="shadow-sm border-b bg-white sticky top-0 z-50">
-      <div className="flex justify-between max-w-6xl mx-5 xl:mx-auto">
+    <div className="h-16 shadow-sm border-b bg-white sticky top-0 z-50">
+      <div className="h-full flex items-center justify-between max-w-6xl mx-5 xl:mx-auto">
         {/*left*/}
         <div
           onClick={() => router.push("/")}
-          className="relative hidden lg:inline-grid w-24 cursor-pointer"
+          className="relative lg:inline-block w-24 h-12 cursor-pointer"
         >
           <Image
             src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
@@ -35,19 +36,10 @@ function Header() {
             objectFit="contain"
           />
         </div>
-        <div
-          onClick={() => router.push("/")}
-          className="relative lg:hidden w-10 flex-shrink-0 cursor-pointer"
-        >
-          <Image
-            src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3e/Instagram_simple_icon.svg/800px-Instagram_simple_icon.svg.png"
-            layout="fill"
-            objectFit="contain"
-          />
-        </div>
+
         {/*middle -- search input field*/}
         <div className="max-w-xs">
-          <div className="relative mt-1 p-3 rounded-md">
+          <div className="hidden sm:block relative mt-1 p-3 rounded-md">
             <div className="absolute inset-y-0 pl-3 flex items-center pointer-events-none">
               <SearchIcon className="h-5 w-5 text-gray-500" />
             </div>
@@ -60,7 +52,17 @@ function Header() {
         </div>
         {/*right -- icons */}
         <div className="flex items-center justify-end space-x-4">
-          <HomeIcon onClick={() => router.push("/")} className="navBtn" />
+          {router.pathname != "/profile" ? (
+            <HomeIconSolid
+              onClick={() => router.push("/")}
+              className="navBtn"
+            />
+          ) : (
+            <HomeIconOutline
+              onClick={() => router.push("/")}
+              className="navBtn"
+            />
+          )}
           {user ? (
             <>
               <div className="relative navBtn ">
--- pages/profile.js
+++ pages/profile.js
@@ -14,12 +14,14 @@ import { auth, db } from "../firebase";
 import { useRecoilState } from "recoil";
 import { userState } from "../atoms/userAtom";
 import { onAuthStateChanged } from "firebase/auth";
+import { useRouter } from "next/router";
 
 export default function profile() {
   const [user, setUser] = useRecoilState(userState);
   const [posts, setPosts] = useState([]);
   const [savedItems, setSavedItems] = useState([]);
   const [selectedContent, setSelectedContent] = useState([]);
+  const router = useRouter();
   useEffect(
     () =>
       onSnapshot(
@@ -63,6 +65,7 @@ export default function profile() {
         setUser(userCopy);
       } else {
         setUser(null);
+        router.pathname === "/profile" && router.push("/");
       }
     });
     return authSubscriber;
